2c54e8e76c82be45ce3a5b409b1444fd68f95004
Bllen Mehary <>
2023-06-10 05:37:32 -0700

    cp

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -17,4 +17,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- move half page up/down and center cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
