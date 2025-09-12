77a4abd5bcae465af4a620ca7cd0713dc26dfc70
Bllen Mehary <>
2025-09-11 21:23:50 -0700

    cp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,17 @@
 
-require('core')
+-- To integrate with VSCode, make sure to point to the correct instance of nvim using the `which nvim` command
+-- Remember to install lua-language-server via Mason in neovim standalone
 
+print("Initializing neovim")
 
+if vim.g.vscode then
+    -- VSCode extension
+    print("Loading Neovim configuration for VS Code...")
+else
+    -- ordinary Neovim
+    print("Loading Neovim configuration for standalone use...")
 
+    require('core')
+end
+
+print("all done")
